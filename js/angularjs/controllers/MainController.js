7877cabde58c0798c4e9da327e55dfea694b369e
Hornetti <filippo.gorini@gmail.com>
2016-05-15 20:50:50 +0200

    add MainController.js
    change link webdoc

--- js/angularjs/controllers/MainController.js
+++ js/angularjs/controllers/MainController.js
@@ -1,18 +1,18 @@
 app.controller('MainController', ['$scope', function($scope) {
 	$scope.Link_Home = { 
 		name : 'Small city, big stories',
-		direct : 'http://localhost/websiteprojects/LOCALESCBS/#/home'
+		direct : 'http://hornetti.github.io/smallcitybigstories/Index.html/home'
 	};
 	$scope.Link_Map = {	
 		name : 'Faenza Metro Map',
-		direct : 'http://localhost/websiteprojects/LOCALESCBS/#/map'
+		direct : 'http://hornetti.github.io/smallcitybigstories/Index.html/map'
 	};
 	$scope.Link_Credits = {
 		name : 'Credits',
-		direct : 'http://localhost/websiteprojects/LOCALESCBS/#/credits'
+		direct : 'http://hornetti.github.io/smallcitybigstories/Index.html/credits'
 	};
 	$scope.Link_Contact = {
 		name : 'Contact us',
-		direct : 'http://localhost/websiteprojects/LOCALESCBS/#/contact'
+		direct : 'http://hornetti.github.io/smallcitybigstories/Index.html/contact'
 	};
 }]);
