2eb7620c75cc6c8e43af91fa8efb2b8509ea12c4
Hornetti <filippo.gorini@gmail.com>
2016-05-30 17:12:41 +0200

    Try

--- js/angularjs/module/MyApp.js
+++ js/angularjs/module/MyApp.js
@@ -4,23 +4,23 @@ app.config(['$routeProvider', '$locationProvider', function($routeProvider, $loc
   $routeProvider
     .when('/home', { 
       controller: 'HomeController', 
-      templateUrl: 'js/angularjs/views/Home.html' 
+      templateUrl: '../js/angularjs/views/Home.html' 
     })
     .when('/map', {
       controller: 'MapController',
-      templateUrl: 'js/angularjs/views/Map.html'
+      templateUrl: '../js/angularjs/views/Map.html'
     })
     .when('/credits', {
       controller: 'CreditsController',
-      templateUrl: 'js/angularjs/views/Credits.html'
+      templateUrl: '../js/angularjs/views/Credits.html'
     })
     .when('/contact', {
       controller: 'ContactController',
-      templateUrl: 'js/angularjs/views/Contact.html'
+      templateUrl: '../js/angularjs/views/Contact.html'
     })
     .when('/map/:name', {
       controller: 'ProfileController',
-      templateUrl: 'js/angularjs/views/Profile.html'
+      templateUrl: '../js/angularjs/views/Profile.html'
     })
     .otherwise({ 
       redirectTo: '/map' 
