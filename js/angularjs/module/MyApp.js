14c29d5cb234299f2a45b03d1949870325f67c6a
Hornetti <filippo.gorini@gmail.com>
2016-06-03 18:56:33 +0200

    add /path on $routeProvider

--- js/angularjs/module/MyApp.js
+++ js/angularjs/module/MyApp.js
@@ -3,8 +3,8 @@ var app = angular.module('MyApp', ['ngRoute']);
 app.config(['$routeProvider', function($routeProvider) {
   $routeProvider
     .when('/home', { 
-      controller: 'HomeController', 
-      templateUrl: 'js/angularjs/views/Home.html' 
+      controller: 'HomeController',
+      templateUrl: 'js/angularjs/views/Home.html'
     })
     .when('/map', {
       controller: 'MapController',
@@ -18,9 +18,41 @@ app.config(['$routeProvider', function($routeProvider) {
       controller: 'ContactController',
       templateUrl: 'js/angularjs/views/Contact.html'
     })
-    .when('/map/:name', {
+    .when('/map/:id/', {
       controller: 'ProfileController',
       templateUrl: 'js/angularjs/views/Profile.html'
+    })
+    .when('/map/:id/service', {
+      controller: 'ProfileController',
+      templateUrl: 'js/angularjs/views/Service.html'
+    })
+    .when('/map/:id/employees', {
+      controller: 'ProfileController',
+      templateUrl: 'js/angularjs/views/Employees.html'
+    })
+    .when('/map/:id/foundation', {
+      controller: 'ProfileController',
+      templateUrl: 'js/angularjs/views/Foundation.html'
+    })
+    .when('/map/:id/revenue', {
+      controller: 'ProfileController',
+      templateUrl: 'js/angularjs/views/Revenue.html'
+    })
+    .when('/map/:id/economic_growth', {
+      controller: 'ProfileController',
+      templateUrl: 'js/angularjs/views/Economic_growth.html'
+    })
+    .when('/map/:id/type', {
+      controller: 'ProfileController',
+      templateUrl: 'js/angularjs/views/Type.html'
+    })
+    .when('/map/:id/reference_market', {
+      controller: 'ProfileController',
+      templateUrl: 'js/angularjs/views/Reference_market.html'
+    })
+    .when('/map/:id/motivations', {
+      controller: 'ProfileController',
+      templateUrl: 'js/angularjs/views/Motivations.html'
     })
     .otherwise({ 
       redirectTo: '/map' 
