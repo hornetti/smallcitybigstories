c7f8bb34d2d5451b012e311263ca216d39196045
Hornetti <filippo.gorini@gmail.com>
2016-06-15 12:11:31 +0200

    add ProfileController.js
    change $scope.legends item

--- js/angularjs/controllers/ProfileController.js
+++ js/angularjs/controllers/ProfileController.js
@@ -5,13 +5,13 @@ app.controller('ProfileController', ['$scope', '$route', 'characters', '$routePa
     	$scope.comparisons = data;
     	$scope.legends = [
 	    	{
-	    		element : " A — C'erano una serie di occasioni che andavano colte"
+	    		element : " A — C'erano occasioni che andavano colte"
 	    	},
 	    	{
 	    		element : "B — È frutto di un'idea personale"
 	    	},
 	    	{
-	    		element : "C — A seguito di una particolare competenza specifica"
+	    		element : "C — A seguito di una competenza specifica"
 	    	},
 	    	{
 	    		element : "D — A seguito di una passione da coltivare"
