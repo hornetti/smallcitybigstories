9a143c7f8753632e85dda730e05097aa3742f316
Hornetti <filippo.gorini@gmail.com>
2016-06-15 11:37:23 +0200

    add ProfileController.js
    add $scope.Legenda and item

--- js/angularjs/controllers/ProfileController.js
+++ js/angularjs/controllers/ProfileController.js
@@ -3,5 +3,25 @@ app.controller('ProfileController', ['$scope', '$route', 'characters', '$routePa
     	$scope.details = data[$routeParams.id];
     	$scope.paramaters = $routeParams.id;
     	$scope.comparisons = data;
+    	$scope.Legenda = [
+	    	{
+	    		uno : " A — C'erano una serie di occasioni che andavano colte"
+	    	},
+	    	{
+	    		due : "B — È frutto di un'idea personale"
+	    	},
+	    	{
+	    		tre : "C — A seguito di una particolare competenza specifica"
+	    	},
+	    	{
+	    		quattro : "D — A seguito di una passione da coltivare"
+	    	},
+	    	{
+	    		startup : "* startup"
+	    	},
+	    	{
+	    		artigiani : "** artigiani"
+	    	}
+	    ];
   	});
 }]);
