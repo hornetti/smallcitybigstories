e3456d3904130914dcad8d3d99e01280c5e4ba63
Hornetti <filippo.gorini@gmail.com>
2016-06-17 12:40:53 +0200

    remove linkprofile.js

--- js/angularjs/directives/link-profile.js
+++
@@ -1,9 +0,0 @@
-app.directive('linkProfile', function() {
-  return {
-    restrict: 'E',
-    scope: {
-      profileInfo: '=info'
-    },
-    templateUrl: 'js/angularjs/directives/link-profile.html'
-  }; 
-});
