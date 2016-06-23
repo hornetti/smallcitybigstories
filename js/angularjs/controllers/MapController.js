c1bf21c87f3f6121acbc215567e499cc417f05b2
Hornetti <filippo.gorini@gmail.com>
2016-06-23 18:10:36 +0200

    add MapController.js
    change draw and element

--- js/angularjs/controllers/MapController.js
+++ js/angularjs/controllers/MapController.js
@@ -3,56 +3,12 @@ app.controller('MapController', ['$scope', 'characters', function($scope, charac
     	$scope.interv = data;
 	});
 	angular.element(document).ready(function() {
-		TweenLite.from('.st0', 5, {
-	        drawSVG: 0,
+		TweenLite.from('path', 5, {
+	        drawSVG: "50% 50%",
 	        delay: 0.2
 	    });
-	    TweenLite.from('.st9', 5, {
-	        drawSVG: 0,
-	        delay: 0.3
-	    });
-	    TweenLite.from('.st10', 5, {
-	        drawSVG: 0,
-	        delay: 0.4
-	    });
-	    TweenLite.from('.st12', 5, {
-	        drawSVG: 0,
-	        delay: 0.5
-	    });
-	    TweenLite.from('.st1', 5, {
-	        drawSVG: 0,
-	        delay: 0.2
-	    });
-	    TweenLite.from('.st2', 5, {
-	        drawSVG: 0,
-	        delay: 0.3
-	    });
-	    TweenLite.from('.st3', 5, {
-	        drawSVG: 0,
-	        delay: 0.4
-	    });
-	    TweenLite.from('.st4', 5, {
-	        drawSVG: 0,
-	        delay: 0.5
-	    });
-	    TweenLite.from('.st5', 5, {
-	        drawSVG: 0,
-	        delay: 0.2
-	    });
-	    TweenLite.from('.st6', 5, {
-	        drawSVG: 0,
-	        delay: 0.3
-	    });
-	    TweenLite.from('.st7', 5, {
-	        drawSVG: 0,
-	        delay: 0.4
-	    });
-	    TweenLite.from('.st8', 5, {
-	        drawSVG: 0,
-	        delay: 0.5
-	    });
-	    TweenLite.from('.st11', 5, {
-	        drawSVG: 0,
+		TweenLite.from('circle', 5, {
+	        drawSVG: "50% 50%",
 	        delay: 0.2
 	    });
 	});
