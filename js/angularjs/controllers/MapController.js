019bafff7af34b3e275b11dabacc5a63ac4d9d83
Hornetti <filippo.gorini@gmail.com>
2016-06-24 12:02:29 +0200

    add MapController.js
    change GS and drawSVG

--- js/angularjs/controllers/MapController.js
+++ js/angularjs/controllers/MapController.js
@@ -3,13 +3,21 @@ app.controller('MapController', ['$scope', 'characters', function($scope, charac
     	$scope.interv = data;
 	});
 	angular.element(document).ready(function() {
-		TweenLite.from('path', 5, {
+		TweenLite.from('#lineBlue', 5, {
 	        drawSVG: "50% 50%",
-	        delay: 0.2
+	        delay: 1
 	    });
-		TweenLite.from('circle', 5, {
+	    TweenLite.from('#lineRed', 4, {
 	        drawSVG: "50% 50%",
-	        delay: 0.2
+	        delay: 2
+	    });
+	    TweenLite.from('#lineGreen', 3, {
+	        drawSVG: "50% 50%",
+	        delay: 3
+	    });
+	    TweenLite.from('#lineYellow', 2, {
+	        drawSVG: "50% 50%",
+	        delay: 4
 	    });
 	});
 }]);
