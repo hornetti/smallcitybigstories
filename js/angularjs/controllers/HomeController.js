8ed3d8865823116666fb35b7fcb9669344423118
Hornetti <filippo.gorini@gmail.com>
2016-06-26 20:51:11 +0200

    add HomeController.js
    add GS and DrawSVG

--- js/angularjs/controllers/HomeController.js
+++ js/angularjs/controllers/HomeController.js
@@ -1,3 +1,34 @@
 app.controller('HomeController', ['$scope', function($scope) {
 	$scope.ProjectName = "Small city, big stories";
+	angular.element(document).ready(function() {
+		TweenLite.from('.logo0', 1, {
+		  drawSVG: "0%",
+		  ease: Expo.easeOut,
+		  delay: 1.25
+		});
+		TweenLite.from('.logo4', 1, {
+		  drawSVG: "50% 50%",
+		  ease: Expo.easeInOut
+		});
+		TweenLite.from('.logo6', 1, {
+		  drawSVG: "0%",
+		  ease: Expo.easeInOut,
+		  delay: 0.75
+		});
+		TweenLite.from('.logo7', 1, {
+		  drawSVG: "0%",
+		  ease: Expo.easeOut,
+		  delay: 1.3
+		});
+		TweenLite.from('.logo2', 1, {
+		  opacity: 0,
+		  ease: Expo.easeOut,
+		  delay: 1.3
+		});
+		TweenLite.from('.logo5', 2, {
+		  opacity: 0,
+		  ease: Expo.easeOut,
+		  delay: .5
+		});
+	});
 }]);
