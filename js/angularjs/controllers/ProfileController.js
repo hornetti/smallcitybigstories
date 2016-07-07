94e115bcdaf26738a727ef69efce9104672fb5fb
Hornetti <filippo.gorini@gmail.com>
2016-07-07 18:16:10 +0200

    add ProfileController.js

--- js/angularjs/controllers/ProfileController.js
+++ js/angularjs/controllers/ProfileController.js
@@ -79,4 +79,23 @@ app.controller('ProfileController', ['$scope', '$route', 'characters', '$routePa
 			delay: 2
 		});
 	});
+	angular.element(document).ready(function() {
+		var bars = ['.LinePTEK', '.LineIT', '.LineMAV', '.LineFOOD'];
+		TweenMax.staggerFrom(bars, 2, {
+			drawSVG: 0,
+			delay: 2,
+			ease: Expo.easeOut,
+			force3D: true
+		}, 0.2);
+		TweenLite.from('.greyline', 2, {
+			drawSVG: "0%",
+			ease: Expo.easeOut,
+			delay: 1
+		});
+		TweenLite.from('.greytext', 2, {
+			opacity: 0,
+			ease: Expo.easeOut,
+			delay: 1
+		});
+	});
 }]);
