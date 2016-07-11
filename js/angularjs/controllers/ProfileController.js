f1915a0ac6e6ab34485d45d4e2bedc32e5f1134c
Hornetti <filippo.gorini@gmail.com>
2016-07-11 18:00:52 +0200

    add ProfileController.js
    remove GS and DrawSVG x button

--- js/angularjs/controllers/ProfileController.js
+++ js/angularjs/controllers/ProfileController.js
@@ -127,30 +127,6 @@ app.controller('ProfileController', ['$scope', '$route', 'characters', '$routePa
 		});
 	});
 	angular.element(document).ready(function() {
-		$('#XBtnTesto').mouseover(function() {
-		    TweenLite.to('.stroke', 1, {
-				drawSVG: "50% 50%",
-				ease: Expo.easeOut
-		    });
-		});
-		$('#XBtnTesto').mouseleave(function() {
-		    TweenLite.to('.stroke', 1, {
-				drawSVG: "100%",
-				ease: Expo.easeOut
-		    });
-		});
-		TweenLite.from('.stroke', 2, {
-			drawSVG: "0%",
-			ease: Expo.easeOut,
-			delay: 3
-		});
-		TweenLite.from('.txt', 2, {
-			opacity: 0,
-			ease: Expo.easeOut,
-			delay: 2
-		});
-	});
-	angular.element(document).ready(function() {
 		var logo_globo = ['.Logo1Lines2px', '.Logo1Lines5px'];
 		TweenLite.from(logo_globo, 2, {
 			drawSVG: "0%",
