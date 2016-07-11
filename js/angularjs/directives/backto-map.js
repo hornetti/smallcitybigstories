b57db5350f031c3b15a2fa60e0308311a33f12e0
Hornetti <filippo.gorini@gmail.com>
2016-07-11 17:59:55 +0200

    add backto-map.js
    add GS and DrawSVG

--- js/angularjs/directives/backto-map.js
+++ js/angularjs/directives/backto-map.js
@@ -1,6 +1,30 @@
 app.directive('backtoMap', function() {
-  return {
-    restrict: 'E',
-    templateUrl: 'js/angularjs/directives/backto-map.html'
-  }; 
+	return {
+		restrict: 'E',
+		templateUrl: 'js/angularjs/directives/backto-map.html'
+	};
+	angular.element(document).ready(function() {
+		$('#XBtnTesto').mouseover(function() {
+		    TweenLite.to('.stroke', 1, {
+				drawSVG: "50% 50%",
+				ease: Expo.easeOut
+		    });
+		});
+		$('#XBtnTesto').mouseleave(function() {
+		    TweenLite.to('.stroke', 1, {
+				drawSVG: "100%",
+				ease: Expo.easeOut
+		    });
+		});
+		TweenLite.from('.stroke', 2, {
+			drawSVG: "0%",
+			ease: Expo.easeOut,
+			delay: 3
+		});
+		TweenLite.from('.txt', 2, {
+			opacity: 0,
+			ease: Expo.easeOut,
+			delay: 2
+		});
+	});
 });
