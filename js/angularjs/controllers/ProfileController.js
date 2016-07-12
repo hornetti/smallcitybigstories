f99bd023eea28be2cda34d60ceeb1d23aa7176be
Hornetti <filippo.gorini@gmail.com>
2016-07-12 14:03:16 +0200

    add ProfileController.js
    add GS and DrawSVG

--- js/angularjs/controllers/ProfileController.js
+++ js/angularjs/controllers/ProfileController.js
@@ -23,7 +23,33 @@ app.controller('ProfileController', ['$scope', '$route', 'characters', '$routePa
 	    		element : "** artigiani"
 	    	}
 	    ];
-  	});  	
+  	});
+  	/*** Close Button ***/
+	angular.element(document).ready(function() {
+		$('#XBtnTesto').mouseover(function() {
+		    TweenLite.to('.stroke', 1, {
+				drawSVG: "50% 50%",
+				ease: Expo.easeOut
+		    });
+		});
+		$('#XBtnTesto').mouseleave(function() {
+		    TweenLite.to('.stroke', 1, {
+				drawSVG: "100%",
+				ease: Expo.easeOut
+		    });
+		});
+		TweenLite.from('.stroke', 2, {
+			drawSVG: "0%",
+			ease: Expo.easeOut,
+			delay: 3
+		});
+		TweenLite.from('.txt', 2, {
+			opacity: 0,
+			ease: Expo.easeOut,
+			delay: 2
+		});
+	});
+	/**/
 	angular.element(document).ready(function() {
 		var bars = ['.foundation14', '.foundation21', '.foundation23', '.foundation7'];
 		var bars_dates = ['.foundation8', '.foundation3', '.foundation22', '.foundation12',];
