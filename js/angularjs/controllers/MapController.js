d25c2db9e6bb74d60153591fa352f3a1fcdbe59d
Hornetti <filippo.gorini@gmail.com>
2016-07-12 13:54:34 +0200

    add MapController.js
    add GS and DrawSVG

--- js/angularjs/controllers/MapController.js
+++ js/angularjs/controllers/MapController.js
@@ -24,4 +24,28 @@ app.controller('MapController', ['$scope', 'characters', function($scope, charac
 		    delay: 2
 		});
 	});
+	angular.element(document).ready(function() {
+		$('#XBtnTesto').mouseover(function() {
+		    TweenLite.to('.stroke', 1, {
+				drawSVG: "50% 50%",
+				ease: Expo.easeOut
+		    });
+		});
+		$('#XBtnTesto').mouseleave(function() {
+		    TweenLite.to('.stroke', 1, {
+				drawSVG: "100%",
+				ease: Expo.easeOut
+		    });
+		});
+		TweenLite.from('.stroke', 2, {
+			drawSVG: "0%",
+			ease: Expo.easeOut,
+			delay: 3
+		});
+		TweenLite.from('.txt', 2, {
+			opacity: 0,
+			ease: Expo.easeOut,
+			delay: 2
+		});
+	});
 }]);
