332382ca8875b58c0ca47bb3f21f98fec157553d
Hornetti <filippo.gorini@gmail.com>
2016-07-18 11:59:58 +0200

    add ProfileController.js
    try 1.0.3 chapter markers

--- js/angularjs/controllers/ProfileController.js
+++ js/angularjs/controllers/ProfileController.js
@@ -51,49 +51,6 @@ app.controller('ProfileController', ['$scope', '$route', 'characters', '$routePa
 	});
 	/**/
 	angular.element(document).ready(function() {
-		var v = document.querySelector("video");
-		var t = document.querySelector("track");
-		var b = document.querySelector("#bar");
-
-		v.addEventListener('click',play,false);
-		v.addEventListener('timeupdate',update,false);
-		t.addEventListener('loaded',render,false); // Bug in FF31 MAC: wrong event name
-		t.addEventListener('load',render,false);
-
-		function play() {
-			if (v.paused) {
-				v.play(); 
-			} else { 
-				v.pause(); 
-			}
-		}
-
-		function update() {
-			var p = v.currentTime/v.duration*100;
-			b.background = "linear-gradient(to right, #500 "+p+"%, #000 "+p+"%)";
-		}
-
-		function render() {
-			var c = v.textTracks[0].cues;
-			for (var i=0; i<c.length; i++) {
-				var s = document.createElement("span");
-				s.innerHTML = c[i].text;
-				s.setAttribute('data-start',c[i].startTime);
-				s.width = ((c[i].endTime-c[i].startTime)/888*480-7)+'px';
-				s.addEventListener("click",seek);
-				b.appendChild(s);
-			}
-		}
-
-		function seek(e) {
-			v.currentTime = this.getAttribute('data-start');
-			if (v.paused) { 
-				v.play(); 
-			}
-		}
-	});
-	/**/
-	angular.element(document).ready(function() {
 		var bars = ['.foundation14', '.foundation21', '.foundation23', '.foundation7'];
 		var bars_dates = ['.foundation8', '.foundation3', '.foundation22', '.foundation12',];
 		TweenLite.from('.foundation2', 2, {
