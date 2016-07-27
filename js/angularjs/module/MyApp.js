5ce755fddff64a28c978a6967f91ae9e73ce0724
Hornetti <filippo.gorini@gmail.com>
2016-07-27 18:21:05 +0200

    add MyApp.js

--- js/angularjs/module/MyApp.js
+++ js/angularjs/module/MyApp.js
@@ -1,4 +1,4 @@
-var app = angular.module('MyApp', ['ngRoute', 'ngAnimate']);
+var app = angular.module('MyApp', ['ngRoute']);
 
 app.config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
   $routeProvider
