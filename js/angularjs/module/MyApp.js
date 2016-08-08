1f0eecc708f4a92a06076842bda2d1f8312266e8
Hornetti <filippo.gorini@gmail.com>
2016-08-08 12:46:51 +0200

    add MyApp.js
    remove vjs-video custom directive

--- js/angularjs/module/MyApp.js
+++ js/angularjs/module/MyApp.js
@@ -1,4 +1,4 @@
-var app = angular.module('MyApp', ['ngRoute', 'vjs.video']);
+var app = angular.module('MyApp', ['ngRoute']);
 
 app.config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
   $routeProvider
