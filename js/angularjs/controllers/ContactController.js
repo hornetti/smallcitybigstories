f9fab4180ee87a67b987d65df12a7fb8c0411ae4
Hornetti <filippo.gorini@gmail.com>
2016-08-23 17:55:45 +0200

    add ContactController.js

--- js/angularjs/controllers/ContactController.js
+++ js/angularjs/controllers/ContactController.js
@@ -1,4 +1,4 @@
 app.controller('ContactController', ['$scope', function($scope) {
-	$scope.EmailAddress = "Contact SCBS crew";
+	$scope.EmailAddress = "Contact SCBS";
 	$scope.EmailSubmit = "Submit";
 }]);
