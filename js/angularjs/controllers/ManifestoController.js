8a4b1272ee89027fe40ca523be4f4c70a4b0f8d8
Hornetti <filippo.gorini@gmail.com>
2016-08-26 18:38:06 +0200

    add ManifestoController,js

--- js/angularjs/controllers/ManifestoController.js
+++ js/angularjs/controllers/ManifestoController.js
@@ -5,7 +5,7 @@ app.controller('ManifestoController', ['$scope', function($scope) {
     		paragraf : "Il digitale ha imposto ai media nuovi ritmi e nuove sfide, spingendoli in acque ancora inesplorate; l’ennesima prova che il giornalismo è chiamato a superare non riguarda solamente l’approccio alle nuove tecnologie, ma tocca nel profondo gli stessi ingranaggi che quotidianamente lo definiscono."
     	},
     	{
-    		paragraf : "Crediamo sia necessario ripensare la sfera mediatica, combinando i diversi strumenti che il digitale ci offre in modo da facilitare la comprensione dell’utente. È sbagliato pensare che i lettori su Internet siano interessati solamente ad articoli di impatto, veloci e frivoli: il nostro pubblico cerca più giornalismo immersivo in cui investire tempo e impegno."
+    		paragraf : "Crediamo sia necessario ripensare la sfera mediatica, combinando i diversi strumenti che il digitale ci offre in modo da facilitare la comprensione dell’utente. È sbagliato pensare che i lettori su Internet siano interessati solamente ad articoli di impatto, veloci e frivoli: il nostro pubblico cerca più giornalismo immersivo in cui investire risorse e impegno."
     	},
     	{
     		paragraf : "Per questo abbiamo sposato il linguaggio del visual journalism, che non è solamente “creatività” ma una vera e propria corrente nella quale tutti gli insegnamenti giornalistici vengono criticamente esaminati, e nuovi principi guidano il lavoro della newsroom: forma e contenuto acquisiscono finalmente lo stesso peso e si influenzano a vicenda."
