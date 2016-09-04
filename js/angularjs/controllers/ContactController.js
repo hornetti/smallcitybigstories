08c4f0ac9b1428ee2952971c569eb974f4536e55
Hornetti <filippo.gorini@gmail.com>
2016-09-04 17:27:11 +0200

    add ContactController.js

--- js/angularjs/controllers/ContactController.js
+++ js/angularjs/controllers/ContactController.js
@@ -1,4 +1,30 @@
 app.controller('ContactController', ['$scope', function($scope) {
-	$scope.EmailAddress = "Contact SCBS";
+	$scope.EmailAddress = "Contact Scbs";
 	$scope.EmailSubmit = "Submit";
+	angular.element(document).ready(function() {
+	    $('.ButtonSubmit').mouseover(function() {
+	        $('.ButtonSubmit > p').css({
+	            "opacity": "0",
+	            "transform": "translate3d(160px, 0px, 0px)",
+	            "transition": "all linear 0.2s"
+	        });
+	        $('.ButtonSubmit > svg').css({
+	            "opacity": "1",
+	            "transform": "translate3d(80px, 0px, 0px)",
+	            "transition": "all linear 0.2s"
+	        });
+	    });
+	    $('.ButtonSubmit').mouseleave(function() {
+	        $('.ButtonSubmit > p').css({
+	            "opacity": "1",
+	            "transform": "translate3d(80px, 0px, 0px)",
+	            "transition": "all linear 0.2s"
+	        });
+	        $('.ButtonSubmit > svg').css({
+	            "opacity": "0",
+	            "transform": "translate3d(0px, 0px, 0px)",
+	            "transition": "all linear 0.2s"
+	        });
+	    });
+	});
 }]);
