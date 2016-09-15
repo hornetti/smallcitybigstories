62455b83a30f79482c96bab187166ae57be8dec5
Hornetti <filippo.gorini@gmail.com>
2016-09-15 22:07:51 +0200

    add new controller dataviz *.js

---
+++ js/angularjs/controllers/datavisualization/TypemarketController.js
@@ -0,0 +1,308 @@
+app.controller('TypemarketController', ['$scope', function($scope) {
+  	/*** Close Button ***/
+	angular.element(document).ready(function() {
+		$('#Backto').mouseover(function() {
+		    TweenLite.to('.stroke', 1, {
+				drawSVG: "50% 50%",
+				ease: Expo.easeOut
+		    });
+		});
+		$('#Backto').mouseleave(function() {
+		    TweenLite.to('.stroke', 1, {
+				drawSVG: "100%",
+				ease: Expo.easeOut
+		    });
+		});
+		TweenLite.from('.stroke', 2, {
+			drawSVG: "0%",
+			ease: Expo.easeOut,
+			delay: 3
+		});
+		TweenLite.from('.txt', 2, {
+			opacity: 0,
+			ease: Expo.easeOut,
+			delay: 2
+		});
+		/* ptek */
+		TweenMax.set('#DV7Data3PTEK', {
+			opacity: '0'
+		});
+	    $('#DV7Circ1PTEK').mouseover(function() {
+	        $('#DV7Data3PTEK').css({
+	          opacity: '1'
+	        });
+	        $('#DV7Letter3').css({
+	          opacity: '0'
+	        });
+	    });
+	    $('#DV7Circ1PTEK').mouseleave(function() {
+	        $('#DV7Data3PTEK').css({
+	          opacity: '0'
+	        });
+	        $('#DV7Letter3').css({
+	          opacity: '1'
+	        });
+	    });
+	  /* food */
+			TweenMax.set('#DV7Data3FOOD', {
+				opacity: '0'
+			});
+	    $('#DV7Circ1FOOD').mouseover(function() {
+	        $('#DV7Data3FOOD').css({
+	          opacity: '1'
+	        });
+	        $('#DV7Letter3').css({
+	          opacity: '0'
+	        });
+	    });
+	    $('#DV7Circ1FOOD').mouseleave(function() {
+	        $('#DV7Data3FOOD').css({
+	          opacity: '0'
+	        });
+	        $('#DV7Letter3').css({
+	          opacity: '1'
+	        });
+	    });
+	  /* it */
+			TweenMax.set('#DV7Data3IT', {
+				opacity: '0'
+			});
+	    $('#DV7Circ1IT').mouseover(function() {
+	        $('#DV7Data3IT').css({
+	          opacity: '1'
+	        });
+	        $('#DV7Letter3').css({
+	          opacity: '0'
+	        });
+	    });
+	    $('#DV7Circ1IT').mouseleave(function() {
+	        $('#DV7Data3IT').css({
+	          opacity: '0'
+	        });
+	        $('#DV7Letter3').css({
+	          opacity: '1'
+	        });
+	    });
+	  /* mav */
+			TweenMax.set('#DV7Data3MAV', {
+				opacity: '0'
+			});
+	    $('#DV7Circ1MAV').mouseover(function() {
+	        $('#DV7Data3MAV').css({
+	          opacity: '1'
+	        });
+	        $('#DV7Letter3').css({
+	          opacity: '0'
+	        });
+	    });
+	    $('#DV7Circ1MAV').mouseleave(function() {
+	        $('#DV7Data3MAV').css({
+	          opacity: '0'
+	        });
+	        $('#DV7Letter3').css({
+	          opacity: '1'
+	        });
+	    });
+	  /* ptek */
+			TweenMax.set('#DV7Data2PTEK', {
+				opacity: '0'
+			});
+	    $('#DV7Circ2PTEK').mouseover(function() {
+	        $('#DV7Data2PTEK').css({
+	          opacity: '1'
+	        });
+	        $('#DV7Letter2').css({
+	          opacity: '0'
+	        });
+	    });
+	    $('#DV7Circ2PTEK').mouseleave(function() {
+	        $('#DV7Data2PTEK').css({
+	          opacity: '0'
+	        });
+	        $('#DV7Letter2').css({
+	          opacity: '1'
+	        });
+	    });
+	  /* food */
+			TweenMax.set('#DV7Data2FOOD', {
+				opacity: '0'
+			});
+	    $('#DV7Circ2FOOD').mouseover(function() {
+	        $('#DV7Data2FOOD').css({
+	          opacity: '1'
+	        });
+	        $('#DV7Letter2').css({
+	          opacity: '0'
+	        });
+	    });
+	    $('#DV7Circ2FOOD').mouseleave(function() {
+	        $('#DV7Data2FOOD').css({
+	          opacity: '0'
+	        });
+	        $('#DV7Letter2').css({
+	          opacity: '1'
+	        });
+	    });
+	  /* it */
+			TweenMax.set('#DV7Data2IT', {
+				opacity: '0'
+			});
+	    $('#DV7Circ2IT').mouseover(function() {
+	        $('#DV7Data2IT').css({
+	          opacity: '1'
+	        });
+	        $('#DV7Letter2').css({
+	          opacity: '0'
+	        });
+	    });
+	    $('#DV7Circ2IT').mouseleave(function() {
+	        $('#DV7Data2IT').css({
+	          opacity: '0'
+	        });
+	        $('#DV7Letter2').css({
+	          opacity: '1'
+	        });
+	    });
+	  /* mav */
+			TweenMax.set('#DV7Data2MAV', {
+				opacity: '0'
+			});
+	    $('#DV7Circ2MAV').mouseover(function() {
+	        $('#DV7Data2MAV').css({
+	          opacity: '1'
+	        });
+	        $('#DV7Letter2').css({
+	          opacity: '0'
+	        });
+	    });
+	    $('#DV7Circ2MAV').mouseleave(function() {
+	        $('#DV7Data2MAV').css({
+	          opacity: '0'
+	        });
+	        $('#DV7Letter2').css({
+	          opacity: '1'
+	        });
+	    });
+	  /* ptek */
+			TweenMax.set('#DV7Data1PTEK', {
+				opacity: '0'
+			});
+	    $('#DV7Circ3PTEK').mouseover(function() {
+	        $('#DV7Data1PTEK').css({
+	          opacity: '1'
+	        });
+	        $('#DV7Letter1').css({
+	          opacity: '0'
+	        });
+	    });
+	    $('#DV7Circ3PTEK').mouseleave(function() {
+	        $('#DV7Data1PTEK').css({
+	          opacity: '0'
+	        });
+	        $('#DV7Letter1').css({
+	          opacity: '1'
+	        });
+	    });
+	  /* food */
+			TweenMax.set('#DV7Data1FOOD', {
+				opacity: '0'
+			});
+	    $('#DV7Circ3FOOD').mouseover(function() {
+	        $('#DV7Data1FOOD').css({
+	          opacity: '1'
+	        });
+	        $('#DV7Letter1').css({
+	          opacity: '0'
+	        });
+	    });
+	    $('#DV7Circ3FOOD').mouseleave(function() {
+	        $('#DV7Data1FOOD').css({
+	          opacity: '0'
+	        });
+	        $('#DV7Letter1').css({
+	          opacity: '1'
+	        });
+	    });
+	  /* it */
+			TweenMax.set('#DV7Data1IT', {
+				opacity: '0'
+			});
+	    $('#DV7Circ3IT').mouseover(function() {
+	        $('#DV7Data1IT').css({
+	          opacity: '1'
+	        });
+	        $('#DV7Letter1').css({
+	          opacity: '0'
+	        });
+	    });
+	    $('#DV7Circ3IT').mouseleave(function() {
+	        $('#DV7Data1IT').css({
+	          opacity: '0'
+	        });
+	        $('#DV7Letter1').css({
+	          opacity: '1'
+	        });
+	    });
+	  /* mav */
+			TweenMax.set('#DV7Data1MAV', {
+				opacity: '0'
+			});
+	    $('#DV7Circ3MAV').mouseover(function() {
+	        $('#DV7Data1MAV').css({
+	          opacity: '1'
+	        });
+	        $('#DV7Letter1').css({
+	          opacity: '0'
+	        });
+	    });
+	    $('#DV7Circ3MAV').mouseleave(function() {
+	        $('#DV7Data1MAV').css({
+	          opacity: '0'
+	        });
+	        $('#DV7Letter1').css({
+	          opacity: '1'
+	        });
+	    });
+		var circles = ['.PTEKCircle', '.FOODCircle', '.ITCircle', '.MAVCircle'];
+		var letters = ['#DV7Letter1', '#DV7Letter2', '#DV7Letter3'];
+		var stats = ['.PTEKstatLine', '.ITstatLine', '.PTEKstatLineAux', '.ITstatLineAux','.MAVstatLine', '.MAVstatLineAux', '.FOODstatLine', '.FOODstatLineAux'];
+
+		TweenMax.from(circles, 2, {
+		  drawSVG: "0%",
+		  ease: Expo.easeOut,
+		  delay: 0.5
+		});
+		
+		TweenMax.from(stats, 2, {
+		  drawSVG: "0%",
+		  ease: Expo.easeOut,
+		  delay: 1.5
+		});
+		
+		TweenMax.from('.Grid2' , 2, {
+		  drawSVG: "0%",
+		  ease: Expo.easeOut,
+		  delay: 0.5
+		});
+		
+		TweenMax.from(letters, 2.5, {
+		  opacity: 0,
+		  ease: Expo.easeOut,
+		  delay: 0.5
+		  
+		});
+		
+		TweenLite.from('#DV7Answers', 2, {
+		  opacity: 0,
+		  ease: Expo.easeOut,
+		  delay: 0.5
+		});
+		
+		TweenMax.from(['#DV7Names', '#DV7LegendLeft'], 2, {
+		  opacity: 0,
+		  ease: Expo.easeOut,
+		  delay: 1.5
+		});
+	});
+	/**/
+}]);
