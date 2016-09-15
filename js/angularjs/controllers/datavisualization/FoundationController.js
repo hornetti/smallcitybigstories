6234081ceb46d4beb5a8bceca6e9c6afd4bd9b0e
Hornetti <filippo.gorini@gmail.com>
2016-09-15 22:08:07 +0200

    add new controller dataviz *.js

---
+++ js/angularjs/controllers/datavisualization/FoundationController.js
@@ -0,0 +1,58 @@
+app.controller('FoundationController', ['$scope', function($scope) {
+  	/*** Close Button ***/
+	angular.element(document).ready(function() {
+		$('#Backto').mouseover(function() {
+		    TweenLite.to('.stroke', 1, {
+				drawSVG: "50% 50%",
+				ease: Expo.easeOut
+		    });
+		});
+		$('#Backto').mouseleave(function() {
+		    TweenLite.to('.stroke', 1, {
+				drawSVG: "100%",
+				ease: Expo.easeOut
+		    });
+		});
+		TweenLite.from('.stroke', 2, {
+			drawSVG: "0%",
+			ease: Expo.easeOut,
+			delay: 3
+		});
+		TweenLite.from('.txt', 2, {
+			opacity: 0,
+			ease: Expo.easeOut,
+			delay: 2
+		});
+		TweenLite.from('.GstatLine', 2, {
+			drawSVG: "0%",
+			ease: Expo.easeOut,
+			delay: 0
+		});
+		var bars = ['.PTEKlabel',  '.ITlabel',  '.MAVlabel',  '.FOODlabel'];
+		TweenMax.staggerFrom(bars, 2, {
+			drawSVG: 0,
+			delay: 1,
+			ease: Expo.easeOut,
+			force3D: true
+		}, 0.2);
+		var bars_dates = [ '.PTEKreadingLine', '.ITreadingLine', '.MAVreadingLine', '.FOODreadingLine'];
+		TweenMax.staggerFrom(bars_dates, 2, {
+			drawSVG: 0,
+			delay: 1,
+			ease: Expo.easeOut,
+			force3D: true
+		}, 0.2);
+		var dates = ['#DV3DatesPTEK','#DV3DatesIT', '#DV3DatesMAV', '#DV3DatesFOOD'];
+		TweenLite.from(dates, 2, {
+			opacity: 0,
+			ease: Expo.easeOut,
+			delay: 1.5
+		});
+		TweenLite.from('.GColor', 2, {
+			opacity: 0,
+			ease: Expo.easeOut,
+			delay: 0
+		});
+	});
+	/**/
+}]);
