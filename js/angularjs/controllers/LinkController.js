4a99373bee56709b5dea778210ab752aa752977e
Hornetti <filippo.gorini@gmail.com>
2016-09-22 18:32:55 +0200

    add LinkController.js

--- js/angularjs/controllers/LinkController.js
+++ js/angularjs/controllers/LinkController.js
@@ -42,29 +42,4 @@ app.controller('LinkController', ['$scope', function($scope) {
 	  	}		
 	});	
 	*/
-	angular.element(document).ready(function() {
-		TweenMax.set('.UnderLine', {
-			drawSVG: "0%"
-		});
-		$('.Faenza').mouseover(function() {
-			TweenMax.from('.UnderLine', 0.4, {
-				drawSVG: "0%",
-				ease: Expo.easeOut
-			});
-			TweenMax.to('.UnderLine', 0.4, {
-				drawSVG: "100%",
-				ease: Expo.easeOut
-			});
-		});
-		$('.Faenza').mouseleave(function() {
-			TweenMax.from('.UnderLine', 0.25, {
-				drawSVG: "100%",
-				ease: Expo.easeOut
-			});
-			TweenMax.to('.UnderLine', 0.25, {
-				drawSVG: "0%",
-				ease: Expo.easeOut
-			});
-		});
-	});
 }]);
