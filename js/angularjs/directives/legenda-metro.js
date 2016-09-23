66bec40aedf7a2c54efa7dd6f83698fc9de6d8a5
Hornetti <filippo.gorini@gmail.com>
2016-09-23 20:40:44 +0200

    add custom directive *.js

--- js/angularjs/directives/legenda-metro.js
+++ js/angularjs/directives/legenda-metro.js
@@ -1,6 +1,18 @@
 app.directive('legendaMetro', function() {
 	return {
 		restrict: 'E',
-		templateUrl: 'js/angularjs/directives/legenda-metro.html'
+		templateUrl: 'js/angularjs/directives/legenda-metro.html',
+	    link: function(scope, element, attr) {
+	    	angular.element(document).ready(function() {
+				var Legenda = ['#Food','#Mav','#Ptek','#It',]
+				TweenMax.staggerFrom(Legenda, 1, {
+				    opacity: 0,
+				    x: '-10px',
+				    ease: Expo.easeOut,
+				    force3D: true,
+				    delay: 1
+				}, 0.25);
+			});
+	    }
 	};
 });
