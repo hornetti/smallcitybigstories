9797571b4964a510c6903fbbb61f6d8ea0ed5288
Hornetti <filippo.gorini@gmail.com>
2016-10-01 15:57:55 +0200

    add controller dataviz *.js

--- js/angularjs/controllers/datavisualization/FoundationController.js
+++ js/angularjs/controllers/datavisualization/FoundationController.js
@@ -1,28 +1,5 @@
 app.controller('FoundationController', ['$scope', function($scope) {
-  	/*** Close Button ***/
 	angular.element(document).ready(function() {
-		$('#Backto').mouseover(function() {
-		    TweenLite.to('.stroke', 1, {
-				drawSVG: "50% 50%",
-				ease: Expo.easeOut
-		    });
-		});
-		$('#Backto').mouseleave(function() {
-		    TweenLite.to('.stroke', 1, {
-				drawSVG: "100%",
-				ease: Expo.easeOut
-		    });
-		});
-		TweenLite.from('.stroke', 2, {
-			drawSVG: "0%",
-			ease: Expo.easeOut,
-			delay: 3
-		});
-		TweenLite.from('.txt', 2, {
-			opacity: 0,
-			ease: Expo.easeOut,
-			delay: 2
-		});
 		TweenLite.from('.GstatLine', 2, {
 			drawSVG: "0%",
 			ease: Expo.easeOut,
