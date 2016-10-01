76a9f54ea5a2b6d1b3653a3dd2e93108217c4fe3
Hornetti <filippo.gorini@gmail.com>
2016-10-01 15:57:49 +0200

    add controller dataviz *.js

--- js/angularjs/controllers/datavisualization/RevenueController.js
+++ js/angularjs/controllers/datavisualization/RevenueController.js
@@ -1,28 +1,5 @@
 app.controller('RevenueController', ['$scope', function($scope) {
-  	/*** Close Button ***/
 	angular.element(document).ready(function() {
-		$('#Backto').mouseover(function() {
-		    TweenMax.to('.stroke', 1, {
-				drawSVG: "50% 50%",
-				ease: Expo.easeOut
-		    });
-		});
-		$('#Backto').mouseleave(function() {
-		    TweenMax.to('.stroke', 1, {
-				drawSVG: "100%",
-				ease: Expo.easeOut
-		    });
-		});
-		TweenMax.from('.stroke', 2, {
-			drawSVG: "0%",
-			ease: Expo.easeOut,
-			delay: 3
-		});
-		TweenMax.from('.txt', 2, {
-			opacity: 0,
-			ease: Expo.easeOut,
-			delay: 2
-		});
 		var bars=['.PTEKstatLine', '.ITstatLine', '.MAVstatLine', '.FOODstatLine',];
 		TweenMax.from('.GstatLine', 2, {
 			drawSVG: "0%",
