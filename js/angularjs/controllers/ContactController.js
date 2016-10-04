372968a248a9ab11cb6acc67b9d545e95aa76d85
Hornetti <filippo.gorini@gmail.com>
2016-10-04 16:37:59 +0200

    add ContactController.js

--- js/angularjs/controllers/ContactController.js
+++ js/angularjs/controllers/ContactController.js
@@ -26,5 +26,16 @@ app.controller('ContactController', ['$scope', function($scope) {
 	            "transition": "all linear 0.2s"
 	        });
 	    });
+	    /*
+		var TXT = ['.Bold32', '.Regular32', '.Bold21', '.Regular21', '.Bold15', '.Regular15', '.Bold12', '.Regular12'];
+				
+		TweenMax.staggerFrom(TXT, 1, {
+		    opacity: 0,
+		    x: '-10px',
+		    ease: Expo.easeOut,
+		    force3D: true,
+		    delay: 1
+		}, 0.25);
+		*/
 	});
 }]);
