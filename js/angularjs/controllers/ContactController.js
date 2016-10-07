c2702efe2b987451b47678c233a94d5806f7f01c
Hornetti <filippo.gorini@gmail.com>
2016-10-07 18:53:43 +0200

    add ContactController.js

--- js/angularjs/controllers/ContactController.js
+++ js/angularjs/controllers/ContactController.js
@@ -5,19 +5,19 @@ app.controller('ContactController', ['$scope', function($scope) {
 	    $('.ButtonSubmit').mouseover(function() {
 	        $('.ButtonSubmit > p').css({
 	            "opacity": "0",
-	            "transform": "translate3d(160px, 0px, 0px)",
+	            "transform": "translate3d(180px, 0px, 0px)",
 	            "transition": "all linear 0.2s"
 	        });
 	        $('.ButtonSubmit > svg').css({
 	            "opacity": "1",
-	            "transform": "translate3d(80px, 0px, 0px)",
+	            "transform": "translate3d(100px, 0px, 0px)",
 	            "transition": "all linear 0.2s"
 	        });
 	    });
 	    $('.ButtonSubmit').mouseleave(function() {
 	        $('.ButtonSubmit > p').css({
 	            "opacity": "1",
-	            "transform": "translate3d(80px, 0px, 0px)",
+	            "transform": "translate3d(100px, 0px, 0px)",
 	            "transition": "all linear 0.2s"
 	        });
 	        $('.ButtonSubmit > svg').css({
