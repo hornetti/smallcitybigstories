228dabe9da9a423288480c218c0b8ff974da0039
Hornetti <filippo.gorini@gmail.com>
2016-10-08 13:19:38 +0200

    add custom directive *.js

--- js/angularjs/directives/bottom-menu.js
+++ js/angularjs/directives/bottom-menu.js
@@ -1,6 +1,11 @@
 app.directive('bottomMenu', function() { 
   	return { 
 	    restrict: 'E', 
-	    templateUrl: 'js/angularjs/directives/bottom-menu.html'
-  	}; 
+	    templateUrl: 'js/angularjs/directives/bottom-menu.html',
+	    link: function(scope, element, attr) {
+	    	angular.element(document).ready(function() {
+	    		
+	    	});
+	    }
+  	};
 });
