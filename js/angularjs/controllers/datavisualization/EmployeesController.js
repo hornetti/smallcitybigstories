e7cbb916007f11f6f3a28dcfa5268a515a1bc75b
Hornetti <filippo.gorini@gmail.com>
2016-10-11 18:11:57 +0200

    add controller dataviz *.js

--- js/angularjs/controllers/datavisualization/EmployeesController.js
+++ js/angularjs/controllers/datavisualization/EmployeesController.js
@@ -1,27 +1,32 @@
 app.controller('EmployeesController', ['$scope', function($scope) {
 	angular.element(document).ready(function() {
-		var bars = ['.PTEKstatLine', '.ITstatLine', '.MAVstatLine', '.FOODstatLine', ];
-		TweenLite.from('.GstatLine', 2, {
-			drawSVG: "0%",
-			ease: Expo.easeOut,
-			delay: 1
-		});
-		TweenMax.staggerFrom(bars, 2, {
+
+		var Bars = ['.PTEKstatLine', '.ITstatLine', '.MAVstatLine', '.FOODstatLine'];
+
+		TweenMax.staggerFrom(Bars, 2, {
 			drawSVG: 0,
 			delay: 2,
 			ease: Expo.easeOut,
 			force3D: true
 		}, 0.2);
+
+		TweenLite.from('.GstatLine', 2, {
+			drawSVG: 0,
+			ease: Expo.easeOut,
+			delay: 1
+		});
+
 		TweenLite.from('.GColor', 2, {
 			opacity: 0,
 			ease: Expo.easeOut,
 			delay: 1
 		});
+
 		TweenLite.from('.BstatLine', 2, {
-			drawSVG: "0%",
+			drawSVG: 0,
 			ease: Expo.easeOut,
 			delay: 2
 		});
+
 	});
-	/**/
 }]);
