8b8223a26bd7eaa233497964191cffd63952f694
Hornetti <filippo.gorini@gmail.com>
2016-10-11 18:12:04 +0200

    add controller dataviz *.js

--- js/angularjs/controllers/datavisualization/FoundationController.js
+++ js/angularjs/controllers/datavisualization/FoundationController.js
@@ -1,35 +1,43 @@
 app.controller('FoundationController', ['$scope', function($scope) {
 	angular.element(document).ready(function() {
+
+		var Bars = ['.PTEKlabel',  '.ITlabel',  '.MAVlabel',  '.FOODlabel'];
+
+		TweenMax.staggerFrom(Bars, 2, {
+			drawSVG: 0,
+			delay: 1,
+			ease: Expo.easeOut,
+			force3D: true
+		}, 0.2);
+
+		var BarsReading = [ '.PTEKreadingLine', '.ITreadingLine', '.MAVreadingLine', '.FOODreadingLine'];
+
+		TweenMax.staggerFrom(BarsReading, 2, {
+			drawSVG: 0,
+			delay: 1,
+			ease: Expo.easeOut,
+			force3D: true
+		}, 0.2);
+
+		var Dates = ['#DV3DatesPTEK','#DV3DatesIT', '#DV3DatesMAV', '#DV3DatesFOOD'];
+
+		TweenLite.from(Dates, 2, {
+			opacity: 0,
+			ease: Expo.easeOut,
+			delay: 1.5
+		});
+
 		TweenLite.from('.GstatLine', 2, {
 			drawSVG: "0%",
 			ease: Expo.easeOut,
 			delay: 0
 		});
-		var bars = ['.PTEKlabel',  '.ITlabel',  '.MAVlabel',  '.FOODlabel'];
-		TweenMax.staggerFrom(bars, 2, {
-			drawSVG: 0,
-			delay: 1,
-			ease: Expo.easeOut,
-			force3D: true
-		}, 0.2);
-		var bars_dates = [ '.PTEKreadingLine', '.ITreadingLine', '.MAVreadingLine', '.FOODreadingLine'];
-		TweenMax.staggerFrom(bars_dates, 2, {
-			drawSVG: 0,
-			delay: 1,
-			ease: Expo.easeOut,
-			force3D: true
-		}, 0.2);
-		var dates = ['#DV3DatesPTEK','#DV3DatesIT', '#DV3DatesMAV', '#DV3DatesFOOD'];
-		TweenLite.from(dates, 2, {
-			opacity: 0,
-			ease: Expo.easeOut,
-			delay: 1.5
-		});
+
 		TweenLite.from('.GColor', 2, {
 			opacity: 0,
 			ease: Expo.easeOut,
 			delay: 0
 		});
+
 	});
-	/**/
 }]);
