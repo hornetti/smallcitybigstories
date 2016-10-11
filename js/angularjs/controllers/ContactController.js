d50e6ec08f70a32a3b6f8d1c46e0a00cbfd70c5c
Hornetti <filippo.gorini@gmail.com>
2016-10-11 18:12:39 +0200

    add controller *.js

--- js/angularjs/controllers/ContactController.js
+++ js/angularjs/controllers/ContactController.js
@@ -1,30 +1,41 @@
 app.controller('ContactController', ['$scope', function($scope) {
+
 	$scope.EmailAddress = "Contatta Scbs";
+
 	$scope.EmailSubmit = "Invia";
+
 	angular.element(document).ready(function() {
+
 	    $('.ButtonSubmit').mouseover(function() {
+
 	        $('.ButtonSubmit > p').css({
 	            "opacity": "0",
 	            "transform": "translate3d(180px, 0px, 0px)",
 	            "transition": "all linear 0.2s"
 	        });
+
 	        $('.ButtonSubmit > svg').css({
 	            "opacity": "1",
 	            "transform": "translate3d(100px, 0px, 0px)",
 	            "transition": "all linear 0.2s"
 	        });
+
 	    });
+
 	    $('.ButtonSubmit').mouseleave(function() {
+
 	        $('.ButtonSubmit > p').css({
 	            "opacity": "1",
 	            "transform": "translate3d(100px, 0px, 0px)",
 	            "transition": "all linear 0.2s"
 	        });
+
 	        $('.ButtonSubmit > svg').css({
 	            "opacity": "0",
 	            "transform": "translate3d(0px, 0px, 0px)",
 	            "transition": "all linear 0.2s"
 	        });
+
 	    });
 	    /*
 		var TXT = ['.Bold32', '.Regular32', '.Bold21', '.Regular21', '.Bold15', '.Regular15', '.Bold12', '.Regular12'];
@@ -38,4 +49,5 @@ app.controller('ContactController', ['$scope', function($scope) {
 		}, 0.25);
 		*/
 	});
+	
 }]);
