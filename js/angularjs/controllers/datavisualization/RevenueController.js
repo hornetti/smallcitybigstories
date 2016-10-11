d750de75ac150d17ff2f329bdf64430d7d0163dc
Hornetti <filippo.gorini@gmail.com>
2016-10-11 18:12:19 +0200

    add controller dataviz *.js

--- js/angularjs/controllers/datavisualization/RevenueController.js
+++ js/angularjs/controllers/datavisualization/RevenueController.js
@@ -1,31 +1,38 @@
 app.controller('RevenueController', ['$scope', function($scope) {
 	angular.element(document).ready(function() {
-		var bars=['.PTEKstatLine', '.ITstatLine', '.MAVstatLine', '.FOODstatLine',];
+
 		TweenMax.from('.GstatLine', 2, {
 			drawSVG: "0%",
 			ease: Expo.easeOut,
 			delay: 1
 		});
-		TweenMax.staggerFrom(bars, 2, {
+
+		var Bars = ['.PTEKstatLine', '.ITstatLine', '.MAVstatLine', '.FOODstatLine'];
+
+		TweenMax.staggerFrom(Bars, 2, {
 			drawSVG:0, 
 			delay:2, 
 			ease:Expo.easeOut, 
 			force3D:true
 		}, 0.2);
+
 		TweenMax.from(['#DV1NoDataTampieri', '#DV1NoDataBorgini'], 2, {
 			opacity: 0,
 			ease: Expo.easeOut,
 			delay: 2.7
 		});
+
 		TweenMax.from('.GColor', 2, {
 			opacity: 0,
 			ease: Expo.easeOut,
 			delay: 1
 		});
+
 		TweenMax.from('.BstatLine', 2, {
 			drawSVG: "0%",
 			ease: Expo.easeOut,
 			delay: 2
 		});
+
 	});
 }]);
