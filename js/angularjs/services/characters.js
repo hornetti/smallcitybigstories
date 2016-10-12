8e195614f519e97d0ca2e54eb7757878dbb84568
Hornetti <filippo.gorini@gmail.com>
2016-10-12 23:29:08 +0200

    add characters.js

--- js/angularjs/services/characters.js
+++ js/angularjs/services/characters.js
@@ -1,9 +1,13 @@
 app.factory('characters', ['$http', function($http) {
+	
 	return $http.get('http://hornetti.github.io/smallcitybigstories/js/angularjs/services/characters.json')
+
 	.success(function(data) {
 		return data;
 	})
+
 	.error(function(data) {
 		return data;
 	});
+
 }]);
