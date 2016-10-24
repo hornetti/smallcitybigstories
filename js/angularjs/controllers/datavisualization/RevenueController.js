49103928210f90e6c749481888c3791815d908ec
Hornetti <filippo.gorini@gmail.com>
2016-10-24 18:44:54 +0200

    add controller dataviz *.js

--- js/angularjs/controllers/datavisualization/RevenueController.js
+++ js/angularjs/controllers/datavisualization/RevenueController.js
@@ -1,13 +1,13 @@
 app.controller('RevenueController', ['$scope', function($scope) {
 	angular.element(document).ready(function() {
 
-		TweenMax.from('.GstatLine', 2, {
+		TweenMax.from('.GStroke', 2, {
 			drawSVG: "0%",
 			ease: Expo.easeOut,
 			delay: 1
 		});
 
-		var Bars = ['.PTEKstatLine', '.ITstatLine', '.MAVstatLine', '.FOODstatLine'];
+		var Bars = ['.PTEKStroke', '.ITStroke', '.MAVStroke', '.FOODStroke'];
 
 		TweenMax.staggerFrom(Bars, 2, {
 			drawSVG:0, 
@@ -16,22 +16,23 @@ app.controller('RevenueController', ['$scope', function($scope) {
 			force3D:true
 		}, 0.2);
 
-		TweenMax.from(['#DV1NoDataTampieri', '#DV1NoDataBorgini'], 2, {
-			opacity: 0,
-			ease: Expo.easeOut,
-			delay: 2.7
-		});
 
-		TweenMax.from('.GColor', 2, {
+		TweenLite.from('.GColor', 2, {
 			opacity: 0,
 			ease: Expo.easeOut,
 			delay: 1
 		});
 
-		TweenMax.from('.BstatLine', 2, {
+		TweenMax.from('.BStroke', 2, {
 			drawSVG: "0%",
 			ease: Expo.easeOut,
 			delay: 2
+		});
+
+		TweenMax.from(['#DV1NoDataTampieri', '#DV1NoDataBorgini'], 2, {
+			opacity: 0,
+			ease: Expo.easeOut,
+			delay: 2.7
 		});
 
 	});
