bbb5a5b49c084f1e1ef9ca7cb3a3a34438db19d0
Hornetti <filippo.gorini@gmail.com>
2016-10-27 18:46:23 +0200

    add controller dataviz *.js

--- js/angularjs/controllers/datavisualization/FoundationController.js
+++ js/angularjs/controllers/datavisualization/FoundationController.js
@@ -10,10 +10,10 @@ app.controller('FoundationController', ['$scope', function($scope) {
 		var Bars = ['.PTEKStroke', '.ITStroke', '.MAVStroke', '.FOODStroke'];
 
 		TweenMax.staggerFrom(Bars, 2, {
-			drawSVG:0, 
-			delay:2, 
-			ease:Expo.easeOut, 
-			force3D:true
+			drawSVG: 0, 
+			delay: 2, 
+			ease: Expo.easeOut, 
+			force3D: true
 		}, 0.2);
 
 
@@ -21,12 +21,6 @@ app.controller('FoundationController', ['$scope', function($scope) {
 			opacity: 0,
 			ease: Expo.easeOut,
 			delay: 1
-		});
-
-		TweenMax.from('.BStroke', 2, {
-			drawSVG: "0%",
-			ease: Expo.easeOut,
-			delay: 2
 		});
 
 		var BarsReading = ['.PTEKStroke', '.ITStroke', '.MAVStroke', '.FOODStroke'];
