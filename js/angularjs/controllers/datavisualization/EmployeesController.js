004fb2c7da870d7bd326f87932f7f8cca585a926
Hornetti <filippo.gorini@gmail.com>
2016-10-28 16:50:25 +0200

    add controller dataviz *.js

--- js/angularjs/controllers/datavisualization/EmployeesController.js
+++ js/angularjs/controllers/datavisualization/EmployeesController.js
@@ -7,15 +7,14 @@ app.controller('EmployeesController', ['$scope', function($scope) {
 			delay: 1
 		});
 
-		var Bars = ['.PTEKStroke', '.ITStroke', '.MAVStroke', '.FOODStroke'];
+		var Bars = ['.PTEKBars', '.ITBars', '.MAVBars', '.FOODBars'];
 
 		TweenMax.staggerFrom(Bars, 2, {
-			drawSVG:0, 
-			delay:2, 
-			ease:Expo.easeOut, 
-			force3D:true
+			drawSVG: 0, 
+			delay: 2, 
+			ease: Expo.easeOut, 
+			force3D: true
 		}, 0.2);
-
 
 		TweenMax.from('.GColor', 2, {
 			opacity: 0,
