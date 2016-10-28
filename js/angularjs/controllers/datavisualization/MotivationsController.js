12217f63ca797e72015d07ebb7d88ea06f97f8e4
Hornetti <filippo.gorini@gmail.com>
2016-10-28 16:50:31 +0200

    add controller dataviz *.js

--- js/angularjs/controllers/datavisualization/MotivationsController.js
+++ js/angularjs/controllers/datavisualization/MotivationsController.js
@@ -1,137 +1,7 @@
 app.controller('MotivationsController', ['$scope', function($scope) {
 	angular.element(document).ready(function() {
-	    /*
 
-		Data A
-		
-		*/
-		TweenMax.set('#DV4Data1PTEK', {
-			opacity: '0'
-		});
-		TweenMax.set('#DV4Data1MAV', {
-			opacity: '0'
-		});
-		$('#DV4Circ1PTEK').mouseover(function() {
-	        $('#DV4Data1PTEK').css({
-	          opacity: '1',
-	        });
-	    });
-	    $('#DV4Circ1PTEK').mouseleave(function() {
-	        $('#DV4Data1PTEK').css({
-	          opacity: '0'
-	        });
-	    });
-	    $('#DV4Circ1MAV').mouseover(function() {
-	        $('#DV4Data1MAV').css({
-	          opacity: '1'
-	        });
-	    });
-	    $('#DV4Circ1MAV').mouseleave(function() {
-	        $('#DV4Data1MAV').css({
-	          opacity: '0'
-	        });
-	    });
-	    /*
-
-		Data B
-		
-		*/
-		TweenMax.set('#DV4Data2PTEK', {
-			opacity: '0'
-		});
-		TweenMax.set('#DV4Data2IT', {
-			opacity: '0'
-		});
-	    $('#DV4Circ2PTEK').mouseover(function() {
-	        $('#DV4Data2PTEK').css({
-	          opacity: '1'
-	        });
-	    });
-	    $('#DV4Circ2PTEK').mouseleave(function() {
-	        $('#DV4Data2PTEK').css({
-	          opacity: '0'
-	        });
-	    });
-	    $('#DV4Circ2IT').mouseover(function() {
-	        $('#DV4Data2IT').css({
-	          opacity: '1'
-	        });
-	    });
-	    $('#DV4Circ2IT').mouseleave(function() {
-	        $('#DV4Data2IT').css({
-	          opacity: '0'
-	        });
-	    });
-	    /*
-
-		Data C
-		
-		*/
-		TweenMax.set('#DV4Data3IT', {
-			opacity: '0'
-		});
-	    $('#DV4Circ3IT').mouseover(function() {
-	        $('#DV4Data3IT').css({
-	          opacity: '1'
-	        });
-	    });
-	    $('#DV4Circ3IT').mouseleave(function() {
-	        $('#DV4Data3IT').css({
-	          opacity: '0'
-	        });
-	    });
-	    /*
-
-		Data D
-
-		*/
-		TweenMax.set('#DV4Data4PTEK', {
-			opacity: '0'
-		});
-		TweenMax.set('#DV4Data4FOOD', {
-			opacity: '0'
-		});
-		TweenMax.set('#DV4Data4IT', {
-			opacity: '0'
-		});
-	    $('#DV4Circ4PTEK').mouseover(function() {
-	        $('#DV4Data4PTEK').css({
-	          opacity: '1'
-	        });
-	    });
-	    $('#DV4Circ4PTEK').mouseleave(function() {
-	        $('#DV4Data4PTEK').css({
-	          opacity: '0'
-	        });
-	    });
-	    $('#DV4Circ4FOOD').mouseover(function() {
-	        $('#DV4Data4FOOD').css({
-	          opacity: '1'
-	        });
-	    });
-	    $('#DV4Circ4FOOD').mouseleave(function() {
-	        $('#DV4Data4FOOD').css({
-	          opacity: '0'
-	        });
-	    });
-	    $('#DV4Circ4IT').mouseover(function() {
-	        $('#DV4Data4IT').css({
-	          opacity: '1'
-	        });
-	    });
-	    $('#DV4Circ4IT').mouseleave(function() {
-	        $('#DV4Data4IT').css({
-	          opacity: '0'
-	        });
-	    });
-	    /*
-
-		SVG Animation
-
-		*/
-	    var Circles = ['.PTEKStroke', '.ITStroke', '.MAVStroke', '.FOODStroke'];
-
-		var Letters = ['#DV4LetterA', '#DV4LetterB', '#DV4LetterC', '#DV4LetterD'];
+	    var Circles = ['.PTEKCircles', '.ITCircles', '.MAVCircles', '.FOODCircles'];
 
 		TweenMax.from(Circles, 2, {
 			drawSVG: "0%",
@@ -139,35 +9,159 @@ app.controller('MotivationsController', ['$scope', function($scope) {
 			delay: 0.5
 		});
 		
-		TweenMax.from(Letters, 2.5, {
+		TweenMax.from('.Letters', 2.5, {
 			opacity: 0,
 			ease: Expo.easeOut,
 			delay: 0.5
 		});
 
-		TweenMax.from('.WStroke' , 2, {
+		TweenMax.from('.Grid' , 2, {
 			drawSVG: "0%",
 			ease: Expo.easeOut,
 			delay: 0.5
 		});
 
-		TweenMax.from('.WStroke' , 1, {
+		TweenMax.from('.WLines' , 1, {
 			drawSVG: "0%",
 			ease: Expo.easeOut,
 			delay: 1.5
 		});
 
-		TweenLite.from('#DV4Answers', 2, {
+		TweenLite.from('#DV6Answers', 2, {
 			opacity: 0,
 			ease: Expo.easeOut,
 			delay: 1
 		});
 
-		TweenMax.from('#DV4Names', 2, {
+		TweenMax.from('#DV6Names', 2, {
 			opacity: 0,
 			ease: Expo.easeOut,
 			delay: 1.5
 		});
+	    /*
+
+		Motivation Data A
+		
+		*/
+		TweenMax.set('#DV6Data1PTEK', {
+			opacity: '0'
+		});
+		TweenMax.set('#DV6Data1MAV', {
+			opacity: '0'
+		});
+		$('#DV6Circ1PTEK').mouseover(function() {
+	        $('#DV6Data1PTEK').css({
+	          opacity: '1',
+	        });
+	    });
+	    $('#DV6Circ1PTEK').mouseleave(function() {
+	        $('#DV6Data1PTEK').css({
+	          opacity: '0'
+	        });
+	    });
+	    $('#DV6Circ1MAV').mouseover(function() {
+	        $('#DV6Data1MAV').css({
+	          opacity: '1'
+	        });
+	    });
+	    $('#DV6Circ1MAV').mouseleave(function() {
+	        $('#DV6Data1MAV').css({
+	          opacity: '0'
+	        });
+	    });
+	    /*
+
+		Motivation Data B
+		
+		*/
+		TweenMax.set('#DV6Data2PTEK', {
+			opacity: '0'
+		});
+		TweenMax.set('#DV6Data2IT', {
+			opacity: '0'
+		});
+	    $('#DV6Circ2PTEK').mouseover(function() {
+	        $('#DV6Data2PTEK').css({
+	          opacity: '1'
+	        });
+	    });
+	    $('#DV6Circ2PTEK').mouseleave(function() {
+	        $('#DV6Data2PTEK').css({
+	          opacity: '0'
+	        });
+	    });
+	    $('#DV6Circ2IT').mouseover(function() {
+	        $('#DV6Data2IT').css({
+	          opacity: '1'
+	        });
+	    });
+	    $('#DV6Circ2IT').mouseleave(function() {
+	        $('#DV6Data2IT').css({
+	          opacity: '0'
+	        });
+	    });
+	    /*
+
+		Motivation Data C
+		
+		*/
+		TweenMax.set('#DV6Data3IT', {
+			opacity: '0'
+		});
+	    $('#DV6Circ3IT').mouseover(function() {
+	        $('#DV6Data3IT').css({
+	          opacity: '1'
+	        });
+	    });
+	    $('#DV6Circ3IT').mouseleave(function() {
+	        $('#DV6Data3IT').css({
+	          opacity: '0'
+	        });
+	    });
+	    /*
+
+		Motivation Data D
+
+		*/
+		TweenMax.set('#DV6Data4PTEK', {
+			opacity: '0'
+		});
+		TweenMax.set('#DV6Data4FOOD', {
+			opacity: '0'
+		});
+		TweenMax.set('#DV6Data4IT', {
+			opacity: '0'
+		});
+	    $('#DV6Circ4PTEK').mouseover(function() {
+	        $('#DV6Data4PTEK').css({
+	          opacity: '1'
+	        });
+	    });
+	    $('#DV6Circ4PTEK').mouseleave(function() {
+	        $('#DV6Data4PTEK').css({
+	          opacity: '0'
+	        });
+	    });
+	    $('#DV6Circ4FOOD').mouseover(function() {
+	        $('#DV6Data4FOOD').css({
+	          opacity: '1'
+	        });
+	    });
+	    $('#DV6Circ4FOOD').mouseleave(function() {
+	        $('#DV6Data4FOOD').css({
+	          opacity: '0'
+	        });
+	    });
+	    $('#DV6Circ4IT').mouseover(function() {
+	        $('#DV6Data4IT').css({
+	          opacity: '1'
+	        });
+	    });
+	    $('#DV6Circ4IT').mouseleave(function() {
+	        $('#DV6Data4IT').css({
+	          opacity: '0'
+	        });
+	    });
 
 	});
 }]);
