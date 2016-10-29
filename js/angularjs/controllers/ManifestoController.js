7c759687e77ddf89c9cd041eb29f40b9e33102be
Hornetti <filippo.gorini@gmail.com>
2016-10-29 16:11:42 +0200

    add controller *.js

--- js/angularjs/controllers/ManifestoController.js
+++ js/angularjs/controllers/ManifestoController.js
@@ -13,7 +13,7 @@
             paragraf : "È necessario ripensare la sfera mediatica, combinando i diversi strumenti che il digitale ci offre in modo da facilitare la comprensione dell’utente. È sbagliato pensare che i lettori su Internet siano interessati solo ad articoli veloci e d'impatto: il nostro pubblico cerca più giornalismo immersivo in cui investire tempo e impegno."
         },
     	{
-    		paragraf : "È tempo che nelle tradizionali redazioni giornalistiche entrino nuove figure professionali, come visual designer e web developer, e che il loro lavoro venga integrato con quello del giornalista in maniera organica, producendo un feedback continuo e condividendo tutte le parti del processo decisionale."
+    		paragraf : "È tempo che nelle tradizionali newsroom entrino nuove figure professionali, come visual designer e web developer, e che il loro lavoro venga integrato con quello del giornalista in maniera organica, producendo un feedback continuo e condividendo tutte le parti del processo decisionale."
     	}
     ];
     $scope.manifestoCit = "Se è vero che il cambiamento è l’unica costante del giornalismo,";
