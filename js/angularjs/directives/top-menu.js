036edc0ace5ac0442be5a25ffe74f9ea0d737b6a
Hornetti <filippo.gorini@gmail.com>
2016-11-24 16:04:31 +0100

    add custom directive *.js

--- js/angularjs/directives/top-menu.js
+++ js/angularjs/directives/top-menu.js
@@ -3,15 +3,8 @@ app.directive('topMenu', function() {
 	    restrict: 'E', 
 	    templateUrl: 'js/angularjs/directives/top-menu.html',
 	    link: function(scope, element, attr) {
+
 	    	angular.element(document).ready(function() {
-	    		
-				$('#BacktoFaenza').mouseover(function() {
-					$('.Posizione').text('Torna alla mappa');
-				});
-				
-				$('#BacktoFaenza').mouseleave(function() {
-					$('.Posizione').text('Faenza');
-				});
 
 				var effettoClick = document.getElementById("effectClick");
 
