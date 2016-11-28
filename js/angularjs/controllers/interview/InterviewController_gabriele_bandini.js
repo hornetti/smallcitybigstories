53ebf9662670d68e9b859e54a8da3ee3b36a9878
Hornetti <filippo.gorini@gmail.com>
2016-11-28 18:12:41 +0100

    add controller *.js

--- js/angularjs/controllers/interview/InterviewController_gabriele_bandini.js
+++ js/angularjs/controllers/interview/InterviewController_gabriele_bandini.js
@@ -5,7 +5,7 @@ app.controller('InterviewController_gabriele_bandini', ['$scope', '$window', fun
     };
 
     $scope.resetVolume = function() {
-        audioAmbient.volume = 0.4
+        audioAmbient.volume = 0.015;
     }
 
     var videointerview = document.getElementById('VideoIntervista'); 
