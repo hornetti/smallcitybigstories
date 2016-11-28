a64bff3294717349ee7e9089601458c2187c3125
Hornetti <filippo.gorini@gmail.com>
2016-11-28 18:12:28 +0100

    add controller *.js

--- js/angularjs/controllers/interview/InterviewController_anna_tampieri.js
+++ js/angularjs/controllers/interview/InterviewController_anna_tampieri.js
@@ -5,7 +5,7 @@ app.controller('InterviewController_anna_tampieri', ['$scope', '$window', functi
     };
 
     $scope.resetVolume = function() {
-        audioAmbient.volume = 0.4
+        audioAmbient.volume = 0.015;
     }
 
     var videointerview = document.getElementById('VideoIntervista'); 
