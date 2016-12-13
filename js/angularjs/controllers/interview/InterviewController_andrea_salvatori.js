a24918ff58f5dec53d46f9df117d60e4bbf4e9e2
Hornetti <filippo.gorini@gmail.com>
2016-12-13 12:28:25 +0100

    add controllers interview *.js

--- js/angularjs/controllers/interview/InterviewController_andrea_salvatori.js
+++ js/angularjs/controllers/interview/InterviewController_andrea_salvatori.js
@@ -1,6 +1,7 @@
 app.controller('InterviewController_andrea_salvatori', ['$scope', '$window', function($scope, $window) {            
     
     var videointerview = document.getElementById('VideoIntervista'); 
+        videointerview.volume = 0.08;
 
     $scope.PlayPause = function() {
         if (videointerview.paused) {
